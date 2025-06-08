77bafb27d53a43ecbf693fec983a02290dac1f1f
lamujica <35012536+lamujica@users.noreply.github.com>
2025-06-07 20:19:29 -0400

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// /JS for side menu on smaller screens (for smartphones/digital phones)/
+
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector(".navbar");
 let box = document.querySelector(".bx")
