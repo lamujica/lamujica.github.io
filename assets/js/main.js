f46e16f5e7c1d4c543fad6fceb9364447aa6d52a
lamujica <35012536+lamujica@users.noreply.github.com>
2025-06-07 20:06:52 -0400

    Swiper.js Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,4 +34,40 @@ var swiper = new Swiper(".slide-certs", {
     },
   });
 
+  var swiper = new Swiper(".slide-portfolio", {
+    slidesPerView: 3,
+    spaceBetween: 2,
+    centerSlide: "true",
+    grabCursor: "true",
+    loop: true,
+    fade: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1, 
+      },
+      660: {
+        slidesPerView: 2, 
+      },
+      1200: {
+        slidesPerView: 3
+      },
+      1500: {
+        slidesPerView: 4, 
+      },
+      2000: {
+        slidesPerView: 5
+      },
+    },
+  });
+
 
